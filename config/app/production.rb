7c0451c3dc082a0e015d5d7af58d99c6b9db8eb8
Andrei Malyshev <Andrei.Malyshev@kodep.ru>
2014-03-06 03:03:07 +0400

    Fixed faye path

--- config/app/production.rb
+++ config/app/production.rb
@@ -2,4 +2,5 @@ App.configure do
   # Settings specified here will take precedence over those in config/app.rb
 
   # config.key = "value"
+  config.faye_host = "http://dnd.maertech.ru:9295/faye"
 end
