2decdb63dc413ec65274213ea49734eb9d3e0506
Andrei Malyshev <Andrei.Malyshev@kodep.ru>
2014-03-06 03:42:13 +0400

    Added angular_templates.js to assets precompilation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Dnd::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( angular_templates.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
