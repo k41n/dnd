3b2c8108eef133ec6d35542f44da7e223c150cf4
Andrei Malyshev <Andrei.Malyshev@kodep.ru>
2014-03-07 04:27:48 +0400

    Trying to fix game destruction in production mode

--- app/views/api/games/index.jbuilder
+++ app/views/api/games/index.jbuilder
@@ -1,3 +1,1 @@
-json.cache! resource_class.maximum(:updated_at) do
-    json.array! collection.to_a
-end
+json.game resourse
--- config/app/development.rb
+++ config/app/development.rb
@@ -1,3 +1,3 @@
 App.configure do
-  config.faye_host = "http://0.0.0.0:9292/faye"
+  config.faye_host = "http://0.0.0.0:9295/faye"
 end
